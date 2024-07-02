b395849474298fdb43767026a0f28e6965487c68
Ryan Webb <rywebb1998@gmail.com>
2024-07-02 20:33:12 +0100

    remove CardImage from Card scene

--- scripts/card.gd
+++ scripts/card.gd
@@ -6,7 +6,6 @@ extends Node2D
 @export var card_name: String
 @export var card_mana_cost: int
 @export var card_description: String
-@export var card_image: Sprite2D
 
 @onready var name_lbl: Label = $Name/NameLabel
 @onready var mana_lbl: Label = $Mana/ManaLabel
@@ -24,7 +23,6 @@ func set_values(_name: String, _mana_cost: int, _description: String):
 	card_name = _name
 	card_mana_cost = _mana_cost
 	card_description = _description
-	# card_image = image
 
 func _update_graphics():
 	if (name_lbl.get_text() != card_name):
