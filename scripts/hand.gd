19e8259820f054b9a650e562484d8d3e0385eef4
Ryan Webb <rywebb1998@gmail.com>
2024-07-02 21:29:19 +0100

    add cards to hand

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -1,17 +1,17 @@
 @tool
 
+class_name Hand
 extends Node2D
 
 @export var hand_radius: int = 1000
-@export var card_angle: float = 0
+# @export var card_angle: float = 0
+@export var angle_limit: float = 20
+@export var maxmimum_spread_angle: float = 5
 
 @onready var test_card = $TestCard
 @onready var collision_shape: CollisionShape2D = $DebugShape
 
-func get_card_position(angle: float) -> Vector2:
-	var x: float = hand_radius * cos(deg_to_rad(angle - 90))
-	var y: float = hand_radius * sin(deg_to_rad(angle - 90))
-	return Vector2(x, y)
+var hand: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,6 +23,25 @@ func _process(_delta) -> void:
 	# tool logic
 	if (collision_shape.shape as CircleShape2D).radius != hand_radius:
 		(collision_shape.shape as CircleShape2D).radius = hand_radius
-	
-	test_card.position = get_card_position(card_angle)
-	test_card.rotation_degrees = card_angle
+
+func add_card(card: Node2D) -> void:
+	hand.push_back(card)
+	add_child(card)
+	reposition_cards()
+
+func reposition_cards() -> void:
+	var card_spread = min(angle_limit / hand.size(), maxmimum_spread_angle)
+	var current_angle = -(card_spread * (hand.size() - 1)) / 2 - 90
+
+	for card in hand:
+		_update_card_transform(card, current_angle)
+		current_angle += card_spread
+
+func _update_card_transform(card: Node2D, angle_deg: float) -> void:
+	card.position = get_card_position(angle_deg)
+	card.rotation_degrees = angle_deg + 90
+
+func get_card_position(angle_deg: float) -> Vector2:
+	var x: float = hand_radius * cos(deg_to_rad(angle_deg))
+	var y: float = hand_radius * sin(deg_to_rad(angle_deg))
+	return Vector2(x, y)
--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var AttackCardScene: PackedScene = preload ("res://scenes/cards/attack_card.tscn")
 @onready var DefendCardScene: PackedScene = preload ("res://scenes/cards/defend_card.tscn")
 
+@onready var hand: Hand = $CanvasLayer/Hand
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,10 +15,8 @@ func _process(_delta) -> void:
 
 func _on_button_pressed() -> void:
 	var attack_card = AttackCardScene.instantiate()
-	attack_card.position = $CanvasLayer/SpawnPoint.position
-	$CanvasLayer.add_child(attack_card)
+	hand.add_card(attack_card)
 
 func _on_button_2_pressed() -> void:
 	var defend_card = DefendCardScene.instantiate()
-	defend_card.position = $CanvasLayer/SpawnPoint.position
-	$CanvasLayer.add_child(defend_card)
+	hand.add_card(defend_card)
